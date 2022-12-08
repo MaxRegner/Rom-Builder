4582b350952e145ee49ffcf37ecb076d361625ce
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-08 17:00:31 +0100

    Update sparkos.sh

--- sparkos.sh
+++ sparkos.sh
@@ -2,6 +2,9 @@
 repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
 git clone https://github.com/NFS-projects/local_manifest --depth 1 -b spark .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+git clone https://github.com/AOSPA/android_device_sony_kugo device/Sony/kugo
+git clone https://github.com/ExplosiveLobster/platform_vendor_sony_loire vendor/sony/loire
+git clone https://github.com/ExplosiveLobster/kernel_sony_msm-4.9/ kernel/sony/kugo
 
 # build rom
 source build/envsetup.sh
